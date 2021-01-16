aaab1ee749521bc5fdacfd62e0cdef2a2f698701
Jukka Aho <ahojukka5@gmail.com>
2021-01-17 01:06:09 +0700

    Make boot.s bootable

--- boot.s
+++ boot.s
@@ -1,4 +1,8 @@
 .code16 
 .global _start
+
 _start:
   jmp _start
+
+.fill 510 - (. - _start), 1, 0 # add zeroes to make it 510 bytes long 
+.word 0xaa55 # magic bytes that tell BIOS that this is bootable
