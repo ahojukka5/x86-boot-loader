660c368d0c75e0d1fc76befed150591b123343c8
Jukka Aho <ahojukka5@gmail.com>
2021-01-17 02:27:49 +0700

    Print A and halt

--- boot.s
+++ boot.s
@@ -1,8 +1,12 @@
 .code16 
 .global _start
+.intel_syntax noprefix
 
 _start:
-  jmp _start
+  mov   ah, 0x0e
+  mov   al, 0x41
+  int   0x10
+  hlt
 
 .fill 510 - (. - _start), 1, 0 # add zeroes to make it 510 bytes long 
 .word 0xaa55 # magic bytes that tell BIOS that this is bootable
